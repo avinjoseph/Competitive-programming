2a2d9cb2fe52e824865bcdba474e606e44bbfbd9
avinjoseph <avin.chelseafc@gmail.com>
2024-07-10 09:45:39 +0200

    Added 26. Remove Duplicates from Sorted Array

--- LeetCode/Easy/27. Remove Element.py
+++ LeetCode/Easy/27. Remove Element.py
@@ -68,7 +68,7 @@ class Solution:
 
         for i in range(len(nums)):
             if nums[i] != val:
-                nums[i] = nums[k]
+                nums[k] = nums[i]
                 k += 1
 
         return k
